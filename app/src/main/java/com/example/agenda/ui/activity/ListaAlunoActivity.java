5e4bc4439aa461d4ba490b604fd79e2401db0150
douglas.fonseca <douglas.fonseca2010@hotmail.com>
2021-10-18 07:32:33 -0400

    Fundamentos Android.1

---
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunoActivity.java
@@ -0,0 +1,60 @@
+package com.example.agenda.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.agenda.DAO.AlunoDAO;
+import com.example.agenda.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ListaAlunoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Lista de alunos";
+    private final AlunoDAO dao = new AlunoDAO();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+
+        setTitle(TITULO_APPBAR);
+
+        configuraFabNovoAluno();
+    }
+
+    private void configuraFabNovoAluno() {
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_aluno_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abriFormularioAlunoActivity();
+            }
+        });
+    }
+
+    private void abriFormularioAlunoActivity() {
+        startActivity(new Intent(ListaAlunoActivity.this,
+    FormularioAlunoActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        configuraLista();
+    }
+
+    private void configuraLista() {
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dao.todos()));
+    }
+}
